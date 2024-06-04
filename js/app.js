3b94039d8a64cf44c52062f3a30a0db75c6c866c
Pedro Oliveira <designer.pedroliveira@gmail.com>
2024-06-04 14:16:45 -0300

    chore: store input values in objects

--- js/app.js
+++ js/app.js
@@ -9,10 +9,27 @@ const Transaction = {
             document.querySelector('.modal-overlay').classList.remove('openModal');
         },
     },
+    inputs: {
+        description: document.getElementById('description'),
+        value: document.getElementById('value'),
+        category: document.getElementById('category'),
+        date: document.getElementById('date'),
+    },
+    getValues() {
+        return {
+            description: this.inputs.description.value,
+            value: this.inputs.value.value,
+            category: this.inputs.category.value,
+            date: this.inputs.date.value,
+        };
+    },
 };
 
 Transaction.form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const inputValues = Transaction.getValues();
+    console.log(inputValues);
+    Transaction.modal.close();
 });
 
 Transaction.overlay.addEventListener('click', ({ target }) => {
